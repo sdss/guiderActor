720948d3ece75ff9ee7bb21b87edcc7be7b7fd18
José Sánchez-Gallego <gallegoj@uw.edu>
2018-05-30 22:26:25 -0700

    Bump version: 3.9.0dev → 3.9.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ class GuiderActorBuild(build):
 
 
 setup(name='guiderActor',
-      version='3.9.0dev',
+      version='3.9.0',
       description='The SDSS guider actor',
       maintainer='Jose Sanchez-Gallego',
       maintainer_email='gallegoj@uw.edu',
